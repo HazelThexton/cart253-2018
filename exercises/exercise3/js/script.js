8cc470b59dc27db6f2074315d508ab321471164d
Hazel Thexton <hazelthexton@videotron.ca>
2018-09-28 15:55:31 -0400

    EX3: Used loops to condense code and eliminate repetition
    Copied over from exerciseCopy folder used for testing

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -10,38 +10,16 @@ Animal images from:
 https://creativenerds.co.uk/freebies/80-free-wildlife-icons-the-best-ever-animal-icon-set/
 ******************************************************************************/
 
-// Position of the animal we're searching for
+// Position and image of the animal we're searching for
 var targetX = 0;
 var targetY = 0;
-
-// Variables we will assign randomized images to later
 var targetImage;
-var decoyImage1;
-var decoyImage2;
-var decoyImage3;
-var decoyImage4;
-var decoyImage5;
-var decoyImage6;
-var decoyImage7;
-var decoyImage8;
-var decoyImage9;
-var decoyImage10;
 
-// The animal images we will assign to the above variables. Please note I have
-// no idea what those last two animals are so I named them at random.
-var daschund;
-var lion;
-var panther;
-var tiger;
-var leopard;
-var hyena;
-var bear;
-var polarBear;
-var panda;
-var dog;
-var cat;
-// The animal image array.
-var animals = [daschund, lion, panther, tiger, leopard, hyena, bear, polarBear, panda, dog, cat];
+// The initial animal image array.
+var animals = [];
+
+// The animal image array once shuffled
+var shuffledAnimals = [];
 
 // Image of the lost poster background
 var lostPoster;
@@ -81,26 +59,15 @@ var targetSizeIncrease = 2;
 //
 // Loads the target and decoy images before the program starts
 function preload() {
-
   // Assigns each of the animal variables to its corresponding image
   for (var i = 0; i < 11; i++) {
     animals[i] = loadImage("assets/images/" + i + ".png");
   }
-
   // Shuffles the animal array
-  var shuffledAnimals = shuffle(animals);
-  // Assigns a randomly determined animal to each variable including the target
+  shuffledAnimals = shuffle(animals);
+
+  // Assigns a random animal, located first in the array, to the target
   targetImage = shuffledAnimals[0];
-  decoyImage1 = shuffledAnimals[1];
-  decoyImage2 = shuffledAnimals[2];
-  decoyImage3 = shuffledAnimals[3];
-  decoyImage4 = shuffledAnimals[4];
-  decoyImage5 = shuffledAnimals[5];
-  decoyImage6 = shuffledAnimals[6];
-  decoyImage7 = shuffledAnimals[7];
-  decoyImage8 = shuffledAnimals[8];
-  decoyImage9 = shuffledAnimals[9];
-  decoyImage10 = shuffledAnimals[10];
 
   //Preloads the lost poster image
   lostPoster = loadImage("assets/images/target-background.png");
@@ -121,7 +88,6 @@ function setup() {
   // Makes the number of decoys larger or smaller depending on screen size
   // so the difficulty scales accordingly
   numDecoys = windowWidth/12;
-
   // Use a for loop to draw as many decoys as we need
   for (var i = 0; i < numDecoys; i++) {
     // Choose a random location for this decoy
@@ -130,43 +96,18 @@ function setup() {
     // Adds a random positive or negative size variation of 50% relative to
     // default decoy size
     var decoySize = random(0.5, 1.5);
-    // Generate a random number we can use for probability
-    var r = random();
-    // Use the random number to display one of the ten decoy
-    // images, each with a 10% chance of being shown
-    if (r < 0.1) {
-      image(decoyImage1,x,y,decoyImage1.width * decoySize,decoyImage1.height * decoySize);
-    }
-    else if (r < 0.2) {
-      image(decoyImage2,x,y,decoyImage2.width * decoySize,decoyImage2.height * decoySize);
-    }
-    else if (r < 0.3) {
-      image(decoyImage3,x,y,decoyImage3.width * decoySize,decoyImage3.height * decoySize);
-    }
-    else if (r < 0.4) {
-      image(decoyImage4,x,y,decoyImage4.width * decoySize,decoyImage4.height * decoySize);
-    }
-    else if (r < 0.5) {
-      image(decoyImage5,x,y,decoyImage5.width * decoySize,decoyImage5.height * decoySize);
-    }
-    else if (r < 0.6) {
-      image(decoyImage6,x,y,decoyImage6.width * decoySize,decoyImage6.height * decoySize);
-    }
-    else if (r < 0.7) {
-      image(decoyImage7,x,y,decoyImage7.width * decoySize,decoyImage7.height * decoySize);
-    }
-    else if (r < 0.8) {
-      image(decoyImage8,x,y,decoyImage8.width * decoySize,decoyImage8.height * decoySize);
-    }
-    else if (r < 0.9) {
-      image(decoyImage9,x,y,decoyImage9.width * decoySize,decoyImage9.height * decoySize);
-    }
-    else if (r < 1.0) {
-      image(decoyImage10,x,y,decoyImage10.width * decoySize,decoyImage10.height * decoySize);
-    }
+    // Creates a variable that is a random integer from one to 10
+    var r = int(random(1,11));
+    // Uses the r variable to choose a random animal in the array each time,
+    // except the animal at index 0 which is already the target
+    var decoyImage = shuffledAnimals[r];
+    // Displays the chosen animal at the chosen location with the chosen size
+    // modifiers, all randomized
+    image(decoyImage,x,y,128 * decoySize,128 * decoySize);
   }
 
-  // The position of the lost poster background
+  // The position of the lost poster background in the top right,
+  // with a 20 pixel buffer
   var lostPosterX = windowWidth - lostPoster.width/2 - 20;
   var lostPosterY = lostPoster.height/2 + 20;
 
@@ -184,7 +125,7 @@ function setup() {
     // Checks distance between target image and poster again
     var d = dist(lostPosterX,lostPosterY, targetX, targetY);
   }
-  // And draw it (this means it will always be on top)
+  // And draw the target image (this means it will always be on top)
   image(targetImage,targetX,targetY);
 
   // This makes the sine's center equivalent to the target's vertical position
