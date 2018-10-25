c03d99e79ffd1c12b06abdc8d01eab8263783c39
Hazel Thexton <hazelthexton@videotron.ca>
2018-10-25 00:27:59 -0400

    EX6: Added missing parentheses

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -43,8 +43,8 @@ function draw() {
 
   leftPaddle.handleInput();
   rightPaddle.handleInput();
-
-  ball.update;
+//////////////// FIXED
+  ball.update();
   leftPaddle.update();
   rightPaddle.update();
   //////////////// FIXED
